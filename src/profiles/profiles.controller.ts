c6c1fcd2093965ba028cd3d305974f26cd9dc572
Ameer Hamzah Daiyan <ameerhamzahdaiyan.02@gmail.com>
2026-05-18 16:36:24 +0600

    Controller(Get)-AllProfiles done.

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 
 @Controller('profiles') //decorator -> higher order function
-export class ProfilesController {}
+export class ProfilesController {
+    @Get()
+    findAll(@Query('age') age:  number) {
+        return [{ age }];
+    }
+}
