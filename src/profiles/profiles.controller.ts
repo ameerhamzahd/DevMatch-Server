2fbbb726d421f799d6358a7f510df51d25815237
Ameer Hamzah Daiyan <ameerhamzahdaiyan.02@gmail.com>
2026-05-18 16:41:04 +0600

    Controller(Get)-SingleProfile done.

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
 
 @Controller('profiles') //decorator -> higher order function
 export class ProfilesController {
+    // GET /profiles
     @Get()
     findAll(@Query('age') age:  number) {
         return [{ age }];
+    }
+
+    // GET /profiles/:id
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return { id };
     }
 }
